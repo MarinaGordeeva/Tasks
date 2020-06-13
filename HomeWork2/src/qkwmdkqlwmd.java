1b5607882c40866e9dbfebd45180c4d6e726dab5
MarinaGordeeva <58423956+MarinaGordeeva@users.noreply.github.com>
2020-06-14 00:42:30 +0300

    Delete qkwmdkqlwmd.java

--- HomeWork2/src/qkwmdkqlwmd.java
+++
@@ -1,7 +0,0 @@
-public class qkwmdkqlwmd {
-    public static void main(String []args){
-        String line = "aaabccdddc";
-        System.out.println( line.chars().distinct().count() );
-    }
-
-}
