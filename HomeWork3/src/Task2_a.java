5db1eee1b25a5552ae03991e3a75d003b9758fce
MarinaGordeeva <gordeeva1@tut,by>
2020-06-15 23:22:55 +0300

    Сортировка ч помощью оьменов

---
+++ HomeWork3/src/Task2_a.java
@@ -0,0 +1,29 @@
+//Шейкерная сортировка
+
+import java.util.Arrays;
+
+public class Task2_a {
+
+        public static void main(String[] args) {
+            int[] a = {11, 44, 555, 22, 2, 8};
+
+            boolean isSorted = false;
+            int b;
+            while (!isSorted){
+                isSorted = true;
+                //Через цикл for проверяем больше ли элемент следующего элемента;
+                for (int i = 0; i < a.length-1; i++){
+                    //если больше, то массив не отсортирован;
+                    if(a[i] > a [i+1]){
+                        isSorted = false;
+                        //через переменную b переписываем массив в верной последовательности;
+                        b = a[i];
+                        a[i] = a[i+1];
+                        a[i+1] = b;
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(a));
+        }
+
+    }
---
+++ HomeWork3/src/Task2_b.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Task2_b {
+
+    public static void main(String[] args) {
+        int[] mas = {11,44,555,22,2,8,16};
+        int left = 1; // левая граница
+        int right = mas.length - 1; // правая граница
+
+
+     do {
+         for (int i = right; i >= left; i--) { //прохождение цикла слева на право.
+             if (mas[i - 1] > mas[i]) {
+                 int temp = mas[i]; //В переменную temp записывает текущее значение;
+                 mas[i] = mas[i - 1]; //переписываем значение (меняем мстами);
+                 mas[i - 1] = temp;
+             }
+         }
+         //прохождение цикла в обратном направлении (лево на право).
+         left++; //Начинаем прохождение от второго индекса;
+         for (int i = left; i <= right; i++) {
+             if (mas[i - 1] > mas[i]) {
+                 int temp = mas[i];
+                 mas[i] = mas[i - 1];
+                 mas[i - 1] = temp;
+             }
+         }
+         right--;
+     }while (left <= right);
+     System.out.println();
+        System.out.println(Arrays.toString(mas));
+    }
+}
